4ba33af891de83f0f22974600e573d102736ae5d
coaoac <aminec@me.com>
2015-06-21 22:26:42 +0200

    Initial Commit

---
+++ MyTools/MyTools.h
@@ -0,0 +1,19 @@
+//
+//  MyTools.h
+//  MyTools
+//
+//  Created by Amine Chaouki on 21/06/15.
+//  Copyright © 2015 chaouki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyTools.
+FOUNDATION_EXPORT double MyToolsVersionNumber;
+
+//! Project version string for MyTools.
+FOUNDATION_EXPORT const unsigned char MyToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyTools/PublicHeader.h>
+
+
